c4e1a1ee2f4b335b1467a06ba39acffb1839713e
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-01 18:41:03 +0300

    Update welcome message to include new documentation link.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Welcome to our version control system! For more information, please refer to the documentation on our website.');
+    res.send('Welcome to our version control system! For more information, please refer to the documentation on our website: https://example.com/docs.');
 });
 
 app.listen(PORT, () => {
