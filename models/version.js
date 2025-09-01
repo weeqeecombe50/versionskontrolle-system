87901dfbd62edbfad27b23676d0e3a7d38fd790e
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-01 18:37:42 +0300

    feat: initial commit for models/version.js

---
+++ models/version.js
@@ -0,0 +1,9 @@
+class Version {
+    constructor(id, content, timestamp) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+}
+
+module.exports = Version;
