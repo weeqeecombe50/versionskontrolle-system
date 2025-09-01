ea9511f1d2563e5be423dfe5a3c0006f041d5160
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-01 18:39:21 +0300

    Implement version retrieval logic in /api/versions endpoint

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const versions = [
+    { version: '1.0.0', releaseDate: '2023-01-01' },
+    { version: '1.1.0', releaseDate: '2023-06-01' },
+];
+
 app.get('/api/versions', (req, res) => {
-    // Here would be the code to retrieve the versions
-    // TODO: Implement version retrieval logic
-    res.send('Versions list');
+    res.json(versions);
 });
 
 app.listen(PORT, () => {
